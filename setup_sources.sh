9700ded64205106c174b0b1627d19d68bc10cd6e
priontu <61733487+priontu@users.noreply.github.com>
2023-04-02 16:10:33 +0200

    Delete setup_sources.sh

--- setup_sources.sh
+++
@@ -1,5 +0,0 @@
-sudo echo "deb http://mirror-osd.de.bosch.com/ubuntu focal main restricted universe multiverse" > /etc/apt/sources.list
-sudo echo "deb http://mirror-osd.de.bosch.com/ubuntu focal-security main restricted universe multiverse" >> /etc/apt/sources.list
-sudo echo "deb http://mirror-osd.de.bosch.com/ubuntu focal-updates main restricted universe multiverse" >> /etc/apt/sources.list
-# RUN echo "deb http://mirror-osd.de.bosch.com/osd focal main" >> /etc/apt/sources.list
-sudo echo "deb http://mirror-osd.de.bosch.com/partner focal partner" >> /etc/apt/sources.list
